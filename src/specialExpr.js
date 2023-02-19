63ae4db6d1d84342314ccdd996327bf27cb891e0
John Eisenmann <esperantanaso@gmail.com>
2023-02-18 21:57:12 -0500

    Parse all of the specials

--- src/specialExpr.js
+++ src/specialExpr.js
@@ -20,8 +20,97 @@ export class ListExpr extends SpecialExpr {
     }
 }
 
+export class ListTypeExpr extends SpecialExpr {
+    
+    init(parser) {
+        this.attrStmtSeq = parser.readAttrStmtSeq();
+        this.exprSeq = parser.readExprSeq();
+    }
+}
+
+export class FuncExpr extends SpecialExpr {
+    
+    init(parser) {
+        this.attrStmtSeq = parser.readAttrStmtSeq();
+        this.bhvrStmtSeq = parser.readBhvrStmtSeq();
+    }
+}
+
+export class FuncTypeExpr extends SpecialExpr {
+    
+    init(parser) {
+        this.attrStmtSeq = parser.readAttrStmtSeq();
+    }
+}
+
+export class AwaitExpr extends SpecialExpr {
+    
+    init(parser) {
+        this.exprSeq = parser.readExprSeq();
+    }
+}
+
+export class ObjExpr extends SpecialExpr {
+    
+    init(parser) {
+        this.exprSeq = parser.readExprSeq();
+    }
+}
+
+export class ObjTypeExpr extends SpecialExpr {
+    
+    init(parser) {
+        this.exprSeq = parser.readExprSeq();
+    }
+}
+
+export class NominalTypeExpr extends SpecialExpr {
+    
+    init(parser) {
+        this.exprSeq = parser.readExprSeq();
+    }
+}
+
+export class AttrsSpecialExpr extends SpecialExpr {
+    
+    init(parser) {
+        this.attrStmtSeq = parser.readAttrStmtSeq();
+    }
+}
+
+export class DictExpr extends AttrsSpecialExpr {}
+
+export class DictTypeExpr extends AttrsSpecialExpr {}
+
+export class MethodTypeExpr extends AttrsSpecialExpr {}
+
+export class InterfaceTypeExpr extends AttrsSpecialExpr {}
+
+export class FeatureExpr extends AttrsSpecialExpr {}
+
+export class FeatureTypeExpr extends AttrsSpecialExpr {}
+
+export class BundleExpr extends AttrsSpecialExpr {}
+
+export class BundleTypeExpr extends AttrsSpecialExpr {}
+
 export const specialConstructorMap = {
     list: ListExpr,
+    listT: ListTypeExpr,
+    dict: DictExpr,
+    dictT: DictTypeExpr,
+    func: FuncExpr,
+    funcT: FuncTypeExpr,
+    methodT: MethodTypeExpr,
+    await: AwaitExpr,
+    interfaceT: InterfaceTypeExpr,
+    feature: FeatureExpr,
+    featureT: FeatureTypeExpr,
+    bundle: BundleExpr,
+    bundleT: BundleTypeExpr,
+    obj: ObjExpr,
+    objT: ObjTypeExpr,
+    nominalT: NominalTypeExpr,
 };
 
 
